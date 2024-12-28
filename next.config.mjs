b71672bb2b1ed7d3eac2ba382717ea9b7800a628
Pamela Arteaga Campos <pamelaarteaga@MacBook-Pro-de-Pamela.local>
2024-12-28 03:04:44 -0300

    fix: style in build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    reactRoot: true,
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
